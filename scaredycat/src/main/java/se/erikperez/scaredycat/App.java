51da9e72a9befd8a916a3841b504ca329827d5e7
ADMIN <ADMIN@192.168.0.19>
2016-09-29 22:59:23 +0200

    First Commit - First structure of the game

---
+++ scaredycat/src/main/java/se/erikperez/scaredycat/App.java
@@ -0,0 +1,23 @@
+package se.erikperez.scaredycat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public final class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		AnchorPane root = FXMLLoader.load(getClass().getClassLoader().getResource("gui/Main.fxml"));
+		Scene scene = new Scene(root, root.getHeight(), root.getWidth());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
