06f2271a4cd11ea2dd2470e8ef87971dd5d462b2
Erik Perez <eritri89@gmail.com>
2016-10-31 16:25:20 +0100

    beans.xml done to generate the Deck from it

--- scaredycat/src/main/java/se/erikperez/scaredycat/model/Deck.java
+++ scaredycat/src/main/java/se/erikperez/scaredycat/model/Deck.java
@@ -1,15 +1,24 @@
 package se.erikperez.scaredycat.model;
 
+import java.util.Collections;
 import java.util.List;
 
-public class Deck {
+public final class Deck {
 
 	private List<Card> cardContainer;
 
-	public Deck(List<Card> cardContainer) {
+	public Deck(){}
+
+	public List<Card> getCardContainer() {
+		return cardContainer;
+	}
+
+	public void setCardContainer(List<Card> cardContainer) {
 		this.cardContainer = cardContainer;
 	}
 	
-	
-	
+	public void shuffle(){
+		Collections.shuffle(getCardContainer());
+	}
+		
 }
